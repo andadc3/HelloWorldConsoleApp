7c325370f0b201acf63a412630b94dcce9551543
Pop, Anda <andadc3@yahoo.com>
2021-09-17 11:00:07 +0300

    Added comment stating reason why automatic build can't get triggered when a push is done to the git repo

--- HelloWorldConsoleApp/Program.cs
+++ HelloWorldConsoleApp/Program.cs
@@ -8,6 +8,11 @@ namespace HelloWorldConsoleApp
     {
       Console.WriteLine("Hello World!");
       Console.WriteLine("Just another change to trigger SCM polling.");
+      /**
+       * Automatic build won't get triggered, because local Jenkins uses localhost, not visible to the internet.
+       * Due to this, when attempting to add the webhook in Github, the payload url for Jenkins wasn't valid.
+       * Payload URL: https://10.5.10.101:8088/github-webhook or https://localhost:8088/github-webhook
+       */
       Console.WriteLine("Just another change to trigger Jenkins build after adding post-receive git hook.");
     }
   }
